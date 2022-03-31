f03c2166b3cd9bd9c1ce3a4d5d47ecd35b742e9b
wwl93 <102734740+wwl93@users.noreply.github.com>
2022-03-31 22:24:44 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://docs.google.com/forms/d/e/1FAIpQLSexZkXXnYZyUvs1CbOJ6IpyTIwl_aCMpG3Tujjn4SNsiMq3pA/viewform?fbclid=IwAR0cDt-3Q3oCzqrcSy0dZbEq1rC-3snhcEs8EGvY4Lnj_OCMpgtqUnnphrM',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdolnLG3eBDR34jBpu-qV0bjkLH_X57Gtm6xkl5XI8cv1oHVQ/viewform?fbclid=IwAR23Z_Nh76h90Xwtd4i9Htl8dyobgonxK7PPz4j0OgfJ-TMkgd4l-PmAqfg',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdK1VDQv_BNpsi8AVGreTHe49OlaaP9FEJapqx8AbAHdy6ejA/viewform?fbclid=IwAR1qAWpLEWflOGTFut0WNj95gdeyBTSw9ddV8SmbIt-lKWpDLimujVPbPaQ'
 ];
